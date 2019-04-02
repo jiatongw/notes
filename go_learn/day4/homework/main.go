b9a446bb0dd78b156ed88b36d7b2553dde20de8b
Jiatong Wang <wangjiatong@vmware.com>
2019-04-02 02:12:32 -0700

    Progress:Day4

--- go_learn/day4/homework/main.go
+++ go_learn/day4/homework/main.go
@@ -1,1 +1,84 @@
+// 大数相加。
+
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	result, _, err := reader.ReadLine()
+	if err != nil {
+		fmt.Println("read from console err: ", err)
+	}
+
+	strSlice := strings.Split(string(result), "+")
+	strNumber1 := strings.TrimSpace(strSlice[0])
+	strNumber2 := strings.TrimSpace(strSlice[1])
+	fmt.Println(addBig(strNumber1, strNumber2))
+
+}
+
+func addBig(str1 string, str2 string) string {
+	if len(str1) == 0 && len(str2) == 0 {
+		return "0"
+	}
+	var index1 = len(str1) - 1
+	var index2 = len(str2) - 1
+	var left int
+	var result string
+
+	for index1 >= 0 && index2 >= 0 {
+		c1 := str1[index1] - '0'
+		c2 := str2[index2] - '0'
+
+		sum := int(c1) + int(c2) + left
+		if sum >= 10 {
+			left = 1
+		} else {
+			left = 0
+		}
+
+		c3 := (sum % 10) + '0'
+		result = fmt.Sprintf("%c%s", c3, result)
+		index1--
+		index2--
+	}
+
+	for index1 >= 0 {
+		c1 := str1[index1] - '0'
+		sum := int(c1) + left
+		if sum >= 10 {
+			left = 1
+		} else {
+			left = 0
+		}
+		c3 := (sum % 10) + '0'
+		result = fmt.Sprintf("%c%s", c3, result)
+
+		index1--
+	}
+
+	for index2 >= 0 {
+		c2 := str2[index2] - '0'
+		sum := int(c2) + left
+		if sum >= 10 {
+			left = 1
+		} else {
+			left = 0
+		}
+		c3 := (sum % 10) + '0'
+		result = fmt.Sprintf("%c%s", c3, result)
+
+		index2--
+	}
+	if left == 1 {
+		result = fmt.Sprintf("1%s", result)
+
+	}
+	return result
+}
