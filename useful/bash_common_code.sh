d0cb466f4b1c2ebf65434d7e931736254fd77c31
Jiatong Wang <wangjiatong@vmware.com>
2020-07-10 00:58:37 -0700

    Add code to guide how to move docker storage to a large space

--- useful/bash_common_code.sh
+++ useful/bash_common_code.sh
@@ -23,3 +23,18 @@ function sha256 {
 
   "${cmd[@]}" "$@"
 }
+
+##############################################################
+# start docker
+# The recommended docker driver is overlay
+# The default storage directory for docker on the vm is /var/lib/docker
+# which is on the root partition. Since the storage on this partition is limited
+# we can move to /dev/sda1 or other storage has bigger space, let's say ${PATH}
+# Groupadd docker
+DOCKER_STORAGE_DIR="${PATH}/docker"
+sudo mkdir -m777 -p ${DOCKER_STORAGE_DIR}
+echo "EXTRA_DOCKER_STORAGE_OPTIONS='-g ${DOCKER_STORAGE_DIR}'" | sudo tee -a /etc/sysconfig/docker-storage-setup
+sudo /usr/bin/systemctl start docker.service
+sudo /usr/bin/systemctl status docker.service
+
+##############################################################
