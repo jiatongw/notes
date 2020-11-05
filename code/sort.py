adf8bf6c32619b5fde7a6c496aa5ac051a2dcbc7
Jiatong Wang <wangjiatong@vmware.com>
2020-11-04 23:52:07 -0800

    Some code memo

---
+++ code/sort.py
@@ -0,0 +1,18 @@
+###### Merge Sort ######
+
+def mergeSort(array, left, right):
+    if not array or len(array) == 0:
+        return array
+
+    l = []
+    if left == right:
+        l.append(array[left])
+        return l
+    mid = (left + right) // 2
+
+    half_left = 
+
+
+
+
+def merge():
