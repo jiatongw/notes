cf2699e281b0db0a9c5b95627491b75e59d2da52
Jiatong Wang <wangjiatong@vmware.com>
2020-11-05 23:16:49 -0800

    Add quick sort code

--- code/sort.py
+++ code/sort.py
@@ -1,4 +1,5 @@
 ###### Merge Sort ######
+#### 每一步，找中点
 
 def mergeSort(array, left, right):
     if not array or len(array) == 0:
@@ -39,7 +40,47 @@ def merge(left_array, right_array):
     return tmp
 
 
+#### Quick sort #####
+#### https://chinese.freecodecamp.org/news/sorting-in-python/ ###
+#### 和上面的merge sort 不同，merge sort 用的是分治法
+#### quick sort 下面的实现，用的是遍历法
+#### quick sort 也有分治法实现的例子，但是空间复杂度太高，效率低
+
+def partition(array, start, end):
+    pivot = array[start]
+    low = start + 1
+    high = end
+
+    while True:
+
+        ## 左右指针往中间扫，发现左边比Pivot值大的，或者右边比pivot小的，就停
+        while low <= high and array[low] <= array[start]:
+            low += 1
+        while low <= high and array[high] > array[start]:
+            high -= 1
+        
+        ## 交换左右指针的值， 保证左边的值都比pivot小，右边的都比pivot大
+        ## 继续循环
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    ## 交换pivot和high指针的值，这样就完成了第一轮：左边的值都比pivot小，右边的都比pivot大
+    array[start], array[high] = array[high], array[start]
+
+    return high ## return 下一个pivot
+
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = partition(array, start, end)
+    quick_sort(array, start, pivot - 1)
+    quick_sort(array, pivot + 1, end)
+
+
+
 if __name__ == '__main__':
     array = [1, 3, 2, 4, 3, 2, 5, 1, 6, 10]
-    result = mergeSort(array, 0, len(array)-1)
-    print(result)
+    # result = mergeSort(array, 0, len(array)-1)
+    quick_sort(array, 0, len(array)-1)
+    print(array)
