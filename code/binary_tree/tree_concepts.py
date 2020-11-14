50ac9853e53a8d546f3531586d4b4a81cee652ee
Jiatong Wang <wangjiatong@vmware.com>
2020-11-13 23:16:20 -0800

    Tree concepts

---
+++ code/binary_tree/tree_concepts.py
@@ -0,0 +1,29 @@
+"""
+undirected group -> connecteed + acyclic
+
+directed rooted tree -> 面试中最常见的形式
+                     -> 从root 可以到达任何点
+                     -> 从root 到任意一点，只有一条path
+
+binary tree: at most two children node
+
+"""
+
+"""
+balanced binary tree: 
+1. if a tree has n number of nodes, and it is balanced, then the height(level) of the tree = O(log(n))
+"""
+
+"""
+complete binary tree: is a binary tree in which every level, except possibly the last, is completely filled,
+                      and all nodes are as far left as possible
+
+complete tree must be a bilanced tree
+
+"""
+
+"""
+
+BST binary search tree
+
+"""
