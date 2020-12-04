cd9336c78268a6bc0ad085160d5d12ff90aa7939
Jiatong Wang <wangjiatong@vmware.com>
2020-12-03 23:11:30 -0800

    Add bfs graph case

---
+++ code/bfs/Dihkstra_algorithm.py
@@ -0,0 +1,11 @@
+'''
+
+1. Usage: Find shortest path cost from a single node(source node) to any other nodes in that graph
+(点到面(==所有点)的最短距离算法)
+2. data structureL priority queue (min_heap)
+3. 解题思路
+    3.1 initial state (start node)
+    3.2 node expansion/generation rule:
+    3.3 termintion condition: 所有点都计算完毕才停止，也就是p_queue 变空
+
+'''
---
+++ code/bfs/graph.py
@@ -0,0 +1,12 @@
+"""
+BFS操作过程：
+
+1. 延展一个Node
+2. generate its neighbor node
+3. reach out to its neighbor node
+
+Date structure: Maintain a FIFO queue, put all generateed nodes in the queue
+
+Termination conditionL do a loop until the queue is emptry
+
+"""
