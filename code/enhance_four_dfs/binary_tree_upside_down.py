fc921e6f83731837b7550e892309f40e6ad556ec
Jiatong Wang <wangjiatong@vmware.com>
2021-01-18 01:03:08 -0800

    Enhaced four - DFS

---
+++ code/enhance_four_dfs/binary_tree_upside_down.py
@@ -0,0 +1,11 @@
+'''
+binary tree upside down
+
+LC 156
+'''
+
+'''
+DFS 基本方法:
+    1. what does it store in each level 每层代表什么意义？一般来讲解题之前就知道DFS要recurse多少层
+    2. how many different states should we try to put on this level? 每层有多少个状态/case 需要retry
+'''
