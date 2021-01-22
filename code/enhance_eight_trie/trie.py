0b13edd88809ab07234985e91212ec469b831fec
Jiatong Wang <wangjiatong@vmware.com>
2021-01-21 22:22:06 -0800

    Enhance eight

---
+++ code/enhance_eight_trie/trie.py
@@ -0,0 +1,26 @@
+'''
+Trie
+
+each node corresponds to a prefix of a word in the dictionary
+
+'''
+
+'''
+
+how will you do to design a dictionary
+
+先问: requirement/use case?
+- search(word)
+- delete
+- add
+- find all words with given prefix
+
+options:
+- hashmap O(m)
+- linkedlist 
+- sorted list O(m logn)
+- Balanced BST O(m logn)
+- Trie
+
+    Trie: search, add, delete 时间复杂度都是O(m), m是单词长度
+'''
